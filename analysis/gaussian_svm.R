5abb59e9f383b4ae024b32132feaab30938b066e
Ben <bendenisshaffer@gmail.com>
2017-12-19 17:30:42 +0300

    rf

---
+++ analysis/gaussian_svm.R
@@ -0,0 +1,91 @@
+require(kernlab)
+require(parallel)
+require(ggthemes)
+require(gridExtra)
+
+train = read.csv("data/clean/train.csv")
+test = read.csv("data/clean/test.csv")
+
+train = train[,c(1:7)]
+test = test[,c(1:7)]
+
+
+cost = c(1,2,4,6,8,10,12,14,16)  #define list of tuning parameters
+
+#function that fits a gaussian svm for different values of cost
+fit_svm = function(cost){
+        gaussian_ksvm <- ksvm(class ~ ., data = train,
+                              kernel = "rbfdot",
+                              kpar = list(sigma=0.05),
+                              C = cost,
+                              cross = 4)
+        return(gaussian_ksvm)
+}
+
+gaussian_svm = mclapply(cost, fit_svm)
+
+save(gaussian_svm, file = "saved_output/objects/gaussian_svm.RData")
+
+
+gaussian_svm_tb_train = function(model){
+        pred_pca_ksvm = predict(model, train[,-7])
+        tb = table(pred_pca_ksvm, train[,7])
+        return(tb)
+}
+
+gaussian_svm_tb_test = function(model){
+        pred_pca_ksvm = predict(model, test[,-7])
+        tb = table(pred_pca_ksvm, test[,7])
+        return(tb)
+}
+
+
+train_confusion_svm = mclapply(gaussian_svm, gaussian_svm_tb_train)
+test_confusion_svm = mclapply(gaussian_svm, gaussian_svm_tb_test)
+
+
+gaussian_testing_FPR_svm = sapply(test_confusion_svm, function(x) 1 -  x[2,2]/(x[2,1]+x[2,2]))
+gaussian_training_FPR_svm = sapply(train_confusion_svm, function(x) 1 - x[2,2]/(x[2,1]+x[2,2]))
+
+df_svm_FPR = data.frame(Error = c(gaussian_testing_FPR_svm,gaussian_training_FPR_svm), 
+                        Set = c(rep("Test",length(cost)),rep("Train",length(cost))),
+                        Cost = rep(cost,2))
+
+g1 = ggplot(df_svm_FPR, aes(x = Cost, y = Error, color = Set)) + 
+        geom_line(size = 1) + 
+        labs(title = "False Positive Rate/\nTesting&Training Sets SVM")+
+        theme_economist()%+replace%theme(legend.position ="bottom",
+                                         legend.direction = "vertical",
+                                         legend.title = element_blank())+scale_color_colorblind()
+
+gaussian_testing_FNR_svm = sapply(test_confusion_svm, function(x) x[1,2]/(x[1,1]+x[1,2]))
+gaussian_training_FNR_svm = sapply(train_confusion_svm, function(x) x[1,2]/(x[1,1]+x[1,2]))
+
+df_svm_FNR = data.frame(Error = c(gaussian_testing_FNR_svm,gaussian_training_FNR_svm), 
+                        Set = c(rep("Test",length(cost)),rep("Train",length(cost))),
+                        Cost = rep(cost,2))
+
+g2 = ggplot(df_svm_FNR, aes(x = Cost, y = Error, color = Set)) + 
+        geom_line(size = 1) + 
+        labs(title = "False Negative Rate/\nTesting&Training Sets SVM")+
+        theme_economist()%+replace%theme(legend.position ="bottom",
+                                         legend.direction = "vertical",
+                                         legend.title = element_blank())+scale_color_colorblind()
+
+
+gaussian_cv_error = sapply(gaussian_svm, function(x) x@cross)
+gaussian_mean_error_train = sapply(gaussian_svm, function(x) x@error)
+gaussian_mean_error_test = sapply(test_confusion_svm, function(x) (x[2,1]+x[1,2])/sum(x))
+
+df_svm_cv_mean = data.frame(Error = c(gaussian_cv_error, gaussian_mean_error_train, gaussian_mean_error_test), 
+                            Type = c(rep("CV",length(cost)), rep("Train",length(cost)),rep("Test",length(cost))),
+                            Cost = rep(cost,3))
+
+g3 = ggplot(df_svm_cv_mean, aes(x = Cost, y = Error, color = Type)) + 
+        geom_line(size = 1) + 
+        labs(title = "SVM Mean and CV error")+
+        theme_economist()%+replace%theme(legend.position ="bottom",
+                                         legend.direction = "vertical",
+                                         legend.title = element_blank())+scale_color_colorblind()
+
+grid.arrange(g1,g2,g3, ncol = 3, nrow = 1)
